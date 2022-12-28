08f6f92f698d5d56f94b41c912b53401f3382c4e
ikkrit <johan.car@protonmail.com>
2022-12-28 23:46:25 +0100

    monster

---
+++ Monster.php
@@ -0,0 +1,12 @@
+<?php
+
+    abstract class Monster
+    {
+        protected $name;
+        protected $class;
+        protected $skills;
+        protected $attack;
+        protected $life;
+    }
+
+?>
