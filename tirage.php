4afc07b5beac7852090908cbf73c8e06153b4be0
ikkrit <johan.car@protonmail.com>
2023-01-01 22:30:55 +0100

    tirage

--- tirage.php
+++ tirage.php
@@ -1,6 +1,19 @@
-<h1>salut</h1>
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/style.css">
+    <title>Tirage</title>
+</head>
 
-    echo $_POST["selection"];
+<body>
 
-?>
+    <h1>Tirage</h1>
+    <h2>Numéros joués</h2>
+
+    <div class="numeros">10</div>
+    
+</body>
+</html>
